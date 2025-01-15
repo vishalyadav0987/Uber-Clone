5dbb2f386013ec5db88898d45a8e0bb351ebac53
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2025-01-15 15:27:03 +0530

    Initialize the backend setup the backend connect with database using mongoose

---
+++ Backend/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+
+
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+
+app.get('/test',(req,res)=>{
+    res.send("This routing using for testing purpouse");
+})
+
+module.exports = app;
+
