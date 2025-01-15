7c855e61e38e6c7ee10b1add2d6f20287a652389
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2025-01-15 16:24:25 +0530

    feat: Add UserSchema with authentication methods
    - Introduced  using Mongoose to define the structure of the user document in MongoDB.
    - Schema includes the following fields:
      - : An object containing:
        - : A required string with a minimum length of 3 characters.
        - : A string with a minimum length of 3 characters.
      - : A required, unique string with a minimum length of 5 characters.
      - : A required string with a minimum length of 8 characters, not selected by default.
      - : A string to store the socket ID for real-time communication.
    
    - Added validation rules to ensure data integrity:
      -  and  must be at least 3 characters long.
      -  must be unique and at least 5 characters long.
      -  must be at least 8 characters long.
    
    - Implemented instance methods and static methods for user authentication:
      - : Generates a JWT token for user authentication using the user's ID and a secret key from environment variables.
      - : A static method to hash user passwords using bcryptjs with a salt round of 10.
      - : Compares a provided password with the hashed password stored in the database using bcryptjs.
    
    - Exported the  model to be used in other parts of the application.
    
    This commit sets up the foundational user model and essential authentication methods required for user management in the application.

---
+++ Backend/models/UserSchema.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const UserSchema = new mongoose.Schema({
+    fullname:{
+        firstname:{
+            type:String,
+            required:true,
+            minlength:[3,"First name must be at least 3 characters"],
+        },firstname:{
+            type:String,
+            minlength:[3,"Last name must be at least 3 characters"],
+        }
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        minlength:[5,"Email must be at least 5 characters long"],
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:[8,"Password must be at least 8 characters long"],
+        select:false,
+        },
+    socketId:{
+        type:String,
+    }
+});
+
+UserSchema.methods.generateAuthToken =()=>{
+    const token = jwt.sign({_id:this.id},process.env.JWT_SECERET);
+    return token;
+}
+
+UserSchema.statics.hashPassword = async(password) =>{
+    return await bcryptjs.hash(password,10);
+}
+
+UserSchema.methods.comparePassword = async(password)=>{
+    return await bcryptjs.compare(password,this.password);
+}
+
+module.exports = mongoose.model("user", UserSchema)
