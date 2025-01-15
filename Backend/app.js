8b421185d71fc984aea71fbd56a1c91849c98b09
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2025-01-15 15:47:17 +0530

    call the coonectDB function in app file for connecting to the database

--- Backend/app.js
+++ Backend/app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const connectDB = require('./connectDB/connect');
 
-
+connectDB();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({extended:true}));
