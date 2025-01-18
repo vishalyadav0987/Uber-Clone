29395922c66585c8e1d4dacff4fd129bda1a35ba
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2025-01-18 14:35:22 +0530

    // Checking for blackKist token 2. because after user logout we need to remove token from black list 3. if user hold the token they can miss use the token

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,12 +1,23 @@
 const UserSchema = require('../models/UserSchema');
 const jwt = require('jsonwebtoken');
+const BlackListToken = require('../models/BlackListToken')
 
 const a = "vishal yadav"
 
 const authUser = async(req,res,next)=>{
-    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
 
     if(!token){
+        return res.status(404).json({success:false,message:"Un-Authorized"});
+    }
+
+    // Checking for blackKist token
+    // because after user logout we need to remove token from black list
+    // if user hold the token they can miss use the token
+
+    const blackListToken = await BlackListToken.findOne({token});
+
+    if(blackListToken){
         return res.status(404).json({success:false,message:"Un-Authorized"});
     }
 
