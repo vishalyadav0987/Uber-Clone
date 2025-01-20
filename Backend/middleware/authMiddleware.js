29ea863c9ad2c827b89488f971ad6be121382c9d
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2025-01-20 15:38:46 +0530

    feat: Implement captain authentication middleware
    - **authMiddleware.js**:
      - Implemented `authCaptain` middleware function to authenticate captains:
        - Extracts the token from cookies or the `Authorization` header.
        - Returns a 401 status code with an "Unauthorized" message if no token is provided.
        - Tries to verify the token using the secret key from environment variables (`process.env.JWT_SECRET`).
        - Decodes the token to get the captain ID and searches for the captain in the database using `CaptainSchema.findById`.
        - Returns a 401 status code with an "Unauthorized" message if the captain does not exist.
        - Attaches the captain object to the `req` object for use in subsequent middleware or route handlers.
        - Calls `next()` to pass control to the next middleware or route handler if authentication is successful.
        - Catches any errors during the process, logs the error message, and returns a 500 status code with an "Unauthorized" message and a success status of `true`.
    
    This commit adds the `authCaptain` middleware function, ensuring that routes are protected and only accessible to authenticated captains.

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 const UserSchema = require('../models/UserSchema');
 const jwt = require('jsonwebtoken');
-const BlackListToken = require('../models/BlackListToken')
+const BlackListToken = require('../models/BlackListToken');
+const CaptainSchema = require('../models/CaptainSchema');
 
 const a = "vishal yadav"
 
@@ -36,6 +37,29 @@ const authUser = async(req,res,next)=>{
 
 }
 
+const authCaptain = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+    try {
+        if (!token) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
+        const captain = await CaptainSchema.findById(decode._id);
+
+        if (!captain) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
+        req.captain = captain;
+        return next();
+    } catch (error) {
+        console.log("Smething went wrong in authCaptain Function ", error.message);
+        return res.status(500).json({ message: "Unauthorized", success: true });
+    }
+}
+
 module.exports = {
-    authUser
+    authUser,
+    authCaptain,
 }
