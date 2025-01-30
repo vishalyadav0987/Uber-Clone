f366a34db2d3d73f4a8d806c02aa6289ef997258
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2025-01-30 16:06:14 +0530

    feat: Implement user login handler in UserLoginPage
    - **UserLoginPage.jsx**:
      - Imported necessary hooks and context:
        - `useNavigate` from `react-router-dom` for navigation.
        - `useUserContext` for accessing user context.
        - `useState` for managing form input states.
      - Defined state variables for `email` and `password` using `useState`.
      - Implemented `loginHandler` function to handle user login:
        - Prevents default form submission behavior.
        - Constructs a `loginUser` object with `email` and `password`.
        - Sends a POST request to the login endpoint (`/api/v1/users/login`) using `axios`.
        - Displays a success toast message and updates user context if login is successful.
        - Sets the authentication token in cookies using `Cookies.set`.
        - Navigates to the home page (`/`) upon successful login.
        - Displays an error toast message if login fails.
        - Logs error details to the console for debugging.
        - Ensures loading state is set to `false` after the API request completes.
    
    This commit adds the user login handler to the UserLoginPage, enabling users to log in and navigate to the home page upon successful authentication.
    
    - Also Set cookies when user register through the credential using js-cookie.

--- Frontend/src/Pages/UserLoginPage/UserLoginPage.jsx
+++ Frontend/src/Pages/UserLoginPage/UserLoginPage.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useUserContext } from '../../Context/userContext'
+import axios from 'axios'
+import toast from 'react-hot-toast'
+import Spinner from '../../Components/Spinner/Spinner'
+import Cookies from 'js-cookie'
 
 const UserLoginPage = () => {
+  const navigate = useNavigate()
+  const { setUser, loading, setLoading } = useUserContext();
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("");
+  const loginHandler = async (e) => {
+    e.preventDefault();
+    const loginUser = {
+      email,
+      password
+    }
+    try {
+      const response = await axios.post('/api/v1/users/login', loginUser);
+      if (response.data && response.data.success) {
+        toast.success(response.data.message, {
+          className: "text-xs"
+        });
+        setUser(response.data.user);
+        Cookies.set('token',response.data?.token);
+        setLoading(false);
+        navigate('/')
+      } else {
+        toast.error(response.data.message, {
+          className: "text-xs"
+        });
+      }
+    } catch (err) {
+      console.log(err.response?.data || err.message); // Log error details
+      const errorMessage = err.response?.data?.message
+        || err.response?.data?.error?.[0]?.msg
+        || err.message
+        || err.response?.data;
+
+      if (errorMessage) {
+        toast.error(errorMessage, { className: "text-xs" });
+      }
+
+    } finally {
+      setLoading(false);
+
+    }
+  }
   return (
     <>
       <div className="h-screen bg-[#212121] w-full">
@@ -10,13 +56,17 @@ const UserLoginPage = () => {
           src="https://freelogopng.com/images/all_img/1659768779uber-logo-white.png"
           alt="" />
         <div className='w-full p-4 flex flex-col justify-between h-[90%]'>
-          <form className='w-full flex flex-col gap-4 mt-4'>
+          <form className='w-full flex flex-col gap-4 mt-4' onSubmit={loginHandler}>
             <div className="flex flex-col gap-2.5 w-full">
               <h2
                 className='font-normal font-sans text-white text-[16px]'>
                 What's you email?
               </h2>
               <input type="email"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value)
+                }}
                 className='p-4 py-2 placeholder:text-xs rounded-sm w-full outline-none'
                 placeholder='Email*'
                 required />
@@ -27,6 +77,10 @@ const UserLoginPage = () => {
                 Password
               </h2>
               <input type="password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value)
+                }}
                 className='p-4 py-2 placeholder:text-xs rounded-sm w-full outline-none'
                 placeholder='Password*'
                 required />
@@ -37,9 +91,11 @@ const UserLoginPage = () => {
                     rounded-sm w-full
                      bg-green-500 
                      text-white cursor-pointer hover:bg-green-700 font-medium'>
-              Login
+              {
+                loading ? (<Spinner/>) : ("Login")
+              }
             </button>
-            <p className='text-white text-xs text-center'>Create a new account? <Link className='underline text-blue-500' to="/user-register">Register</Link></p>
+            <p className='text-white text-xs text-center'>Create a new account? <Link className='text-blue-500' to="/user-register">Register</Link></p>
           </form>
           <div>
             <Link to='/captain-login'
